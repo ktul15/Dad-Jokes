d83a1e11303b1b690cd71f01a34ae2760a997cb0
Ktul <ketulm8@gmail.com>
2020-02-26 15:23:52 +0530

    Add dynamic colors and emojis

--- src/Joke.js
+++ src/Joke.js
@@ -2,21 +2,56 @@ import React, { Component } from "react";
 import "./Joke.css";
 
 class Joke extends Component {
+  getColor() {
+    if (this.props.votes >= 15) {
+      return "#4caf50";
+    } else if (this.props.votes >= 12) {
+      return "#bbc341";
+    } else if (this.props.votes >= 9) {
+      return "#cddc39";
+    } else if (this.props.votes >= 6) {
+      return "#ffeb3b";
+    } else if (this.props.votes >= 3) {
+      return "#ffc107";
+    } else if (this.props.votes >= 0) {
+      return "#ff9800";
+    } else {
+      return "#f44336";
+    }
+  }
+
+  getEmoji() {
+    if (this.props.votes >= 15) {
+      return "em em-rolling_on_the_floor_laughing";
+    } else if (this.props.votes >= 12) {
+      return "em em-laughing";
+    } else if (this.props.votes >= 9) {
+      return "em em-smiley";
+    } else if (this.props.votes >= 6) {
+      return "em em-slightly_smiling_face";
+    } else if (this.props.votes >= 3) {
+      return "em em-neutral_face";
+    } else if (this.props.votes >= 0) {
+      return "em em-confused";
+    } else {
+      return "em em-angry";
+    }
+  }
+
   render() {
+    const { upvote, downvote, votes, text } = this.props;
     return (
       <div className="Joke">
         <div className="Joke-buttons">
-          <i className="fas fa-arrow-up" onClick={this.props.upvote} />
-          <span className="Joke-votes">{this.props.votes}</span>
-          <i className="fas fa-arrow-down" onClick={this.props.downvote} />
+          <i className="fas fa-arrow-up" onClick={upvote} />
+          <span className="Joke-votes" style={{ borderColor: this.getColor() }}>
+            {votes}
+          </span>
+          <i className="fas fa-arrow-down" onClick={downvote} />
         </div>
-        <div className="Joke-text">{this.props.text}</div>
+        <div className="Joke-text">{text}</div>
         <div className="Joke-smiley">
-          <i
-            class="em em-anguished"
-            aria-role="presentation"
-            aria-label="ANGUISHED FACE"
-          />
+          <i className={this.getEmoji()} aria-label="ANGUISHED FACE" />
         </div>
       </div>
     );
